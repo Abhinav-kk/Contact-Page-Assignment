ad2d92e469bfeb694d388f17b4c256aa8edbf2ee
Abhinav K K <91086614+Abhinav-kk@users.noreply.github.com>
2023-05-02 23:34:07 +0400

    Added App Code

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Contact from './screens/Contact';
+import * as Font from "expo-font";
+
 
 export default function App() {
+  const [fontsLoaded] = Font.useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Contact />
   );
 }
 
